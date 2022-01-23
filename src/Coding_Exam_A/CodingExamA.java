f4046cb4eafe87516e98282978c66592676ff2ba
dylanvanwinkle <dylanvanwinkle@gmail.com>
2022-01-23 11:25:36 -0800

    CEAIP

--- src/Coding_Exam_A/CodingExamA.java
+++ src/Coding_Exam_A/CodingExamA.java
@@ -1,6 +1,7 @@
 package Coding_Exam_A;
 
 import java.awt.Color;
+import java.awt.List;
 
 import javax.swing.JOptionPane;
 
@@ -22,5 +23,20 @@ public class CodingExamA {
 		 * See the Coding_Exam_A_Demo.jar for an example of what the finished product should look like.
 		 */
 
+String s = JOptionPane.showInputDialog("How many Robots?");
+String s2 = JOptionPane.showInputDialog("What Color?(Red,Green,Blue)");
+int input = Integer.parseInt(s);
+Thread[] t = new Thread[input];
+for (int i = 0; i < input; i++) {
+	t[i] = new Thread(()-> {Robot r = new Robot();
+	if (s2.equalsIgnoreCase("red")) {
+	r.setPenColor(Color.red);	
+	}else if (s2.equalsIgnoreCase("green")) {
+		r.setPenColor(Color.green);	
+	}else if (s2.equalsIgnoreCase("blue")) {
+		r.setPenColor(Color.blue);
+	}
+	});
+}
 	}
 }
