c1cb342b868c7853e56e5c24047a980ced52c9ff
dylanvanwinkle <dylanvanwinkle@gmail.com>
2022-02-06 11:26:01 -0800

    Coding Part B IP

--- src/Coding_Exam_B/CodingExamB.java
+++ src/Coding_Exam_B/CodingExamB.java
@@ -5,16 +5,16 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.stream.Stream;
 
 public class CodingExamB {
-	/*
+	static /*
 	 * This is a logging program for collecting TODO comments in a program. The program will scan
 	 * Through all the files in the Coding_Exam_B/classes package, and collect all the comments that start
 	 * with //TODO: and write them to their own file. See the TODO_Log_example.txt file for an idea of what 
 	 * the final file output will look like.
 	 */
-	
-	
+	String value = "";
 	public static String getLoggingInfo(String fileName) {
 		/*
 		 * 1. Complete the getLoggingInfoMethod.
@@ -23,18 +23,38 @@ public class CodingExamB {
 		 *    take all the comments that begin with //TODO: and combine them 
 		 *    into one large String. The string will also state the file name and
 		 *    the line number for where each TODO was found. 
+		*/	
+		
+		// My Notes
+		/*
+		trying to get the while loop to run the correct amount of times
+		c2 is so that it will only quit after a todo comment has been read(every class has at least 1)
+		issue is that c2 does not work of classes with multiple todo lines.(some class have 1 todo line, some have 2)
 		*/
-		String s = "";
+		value = fileName + " /n ";
+		boolean tryLine = true;
+		int counter = 0;
+		int c2 = 0;
 		try {
 			FileReader fr = new FileReader(fileName);
 			BufferedReader br = new BufferedReader(fr);
-			if (br.equals("//TODO")) {
-				try {
-				s =	br.readLine();
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
+			try {
+				while(tryLine) {
+					String line = br.readLine();
+					if (line.contains("//TODO")) {
+					value += (counter + 1) + ": " + line + " /n ";	
+						System.out.println(value);
+						c2 ++;
+					}else if (line.isEmpty() && c2 > 0) {
+						c2 = 0;
+						tryLine = false;
+					}
+					counter ++;
 				}
+				
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
 			}
 			
 			
@@ -42,13 +62,12 @@ public class CodingExamB {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		System.out.println(s);
-		return s;
+		return value;
 	}
 	
 	public static void main(String[] args) {
 		String finalLogString = getLoggingInfo("src/Coding_Exam_B/classes/Camera.java");
-		finalLogString += getLoggingInfo("src/Coding_Exam_B/classes/RayTracedImageViewer.java");
+		 finalLogString += getLoggingInfo("src/Coding_Exam_B/classes/RayTracedImageViewer.java");
 		finalLogString += getLoggingInfo("src/Coding_Exam_B/classes/RayTracer.java");
 		finalLogString += getLoggingInfo("src/Coding_Exam_B/classes/Vector3.java");
 		
@@ -56,5 +75,12 @@ public class CodingExamB {
 		 * 2. Write the finalLogString to a file called TODO_Log.txt. The file should match TODO_Log_example.txt. 
 		 */
 
+		try {
+			FileWriter fw = new FileWriter("TODO_Log.txt");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 	}
 }
